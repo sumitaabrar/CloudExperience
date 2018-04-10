feeaa9b90f4897a817352bbbad814d8db0205068
sumitaabrar <38225351+sumitaabrar@users.noreply.github.com>
2018-04-10 18:09:28 +0500

    Add files via upload

---
+++ home.php
@@ -0,0 +1,54 @@
+<!DOCTYPE>
+<html>
+<head>
+   <title>Home</title>
+   <link rel="stylesheet" type="text/css" href="mystyle.css" />
+</head>
+
+<body>
+ <div id="wrapper">
+   <nav>
+	<a href="home.php"> HOME    </a>
+	<a href="cloudGoals.php">CLOUD GOALS </a>
+	<a href="cloudRisks.php">CLOUD RISKS </a>
+	<a href="cloudTactics.php">CLOUD TACTICS </a>
+	<a href="migrationType.php">MIGRATION TYPE </a>
+	<a href="evidence.php">EVIDENCE </a>
+	<a href="autoResolve.php">AUTO RESOLVE </a>
+	<a href="qna.php">Q&A </a>
+	<a href="contactUs.php">CONTACT US </a>
+   </nav>
+
+   <div id="section">
+
+	* <br/>
+	* <br/>
+	* <br/>
+	* <br/>
+	* <br/>
+	* <br/>
+	* <br/>
+	* <br/>
+	* <br/>
+	* <br/>
+	* <br/>
+	* <br/>
+	* <br/>
+	* <br/>
+	* <br/>
+	* <br/>
+
+   </div>
+
+   <footer>
+	<p> Copyrights© CloudExperience. All rights reserved. </p
+   </footer>
+
+ </div>
+   
+
+
+</body>
+
+
+</html>
